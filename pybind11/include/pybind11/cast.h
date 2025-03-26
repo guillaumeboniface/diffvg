8cb5c662e15e951267533fa1ba084cbb5940e628
Guillaume Boniface-Chang <guillaume.boniface@gmail.com>
2025-03-26 16:37:19 +0000

    Finding a way to build on python 3.10

--- pybind11/include/pybind11/cast.h
+++ pybind11/include/pybind11/cast.h
@@ -948,7 +948,7 @@ template <typename type> using make_caster = type_caster<intrinsic_t<type>>;
 
 // Shortcut for calling a caster's `cast_op_type` cast operator for casting a type_caster to a T
 template <typename T> typename make_caster<T>::template cast_op_type<T> cast_op(make_caster<T> &caster) {
-    return caster.operator typename make_caster<T>::template cast_op_type<T>();
+    return caster;
 }
 template <typename T> typename make_caster<T>::template cast_op_type<typename std::add_rvalue_reference<T>::type>
 cast_op(make_caster<T> &&caster) {
